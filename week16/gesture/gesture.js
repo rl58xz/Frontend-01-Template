aa2b919a520f906e6c15b18828e7eccd07930042
renmac <renliang1000@163.com>
2020-09-02 15:15:38 +0800

    添加事件判定逻辑

--- week16/gesture/gesture.js
+++ week16/gesture/gesture.js
@@ -1,18 +1,19 @@
 let element = document.body;
 
-let contexts = new Map();
+let contexts = Object.create(null);
 
 let MOUSE_SYMBOL = Symbol('mouse');
+let HAND_SYMBOL = Symbol('hand');
 
-element.addEventListener('mousedown',(event)=>{
+element.addEventListener('mousedown',(event) => {
     contexts[MOUSE_SYMBOL] = Object.create(null);
     start(event,contexts[MOUSE_SYMBOL]);
     let mousemove = (event)=>{
-        move(event);
+        move(event,contexts[MOUSE_SYMBOL]);
     }
 
     let mouseend = (event) => {
-        end(event);
+        end(event,contexts[MOUSE_SYMBOL]);
         element.removeEventListener('mousemove',mousemove);
         element.removeEventListener('mouseup',mouseend);
     }
@@ -22,39 +23,76 @@ element.addEventListener('mousedown',(event)=>{
 })
 
 element.addEventListener('touchstart',(event)=>{
+    contexts[HAND_SYMBOL] = Object.create(null);
     for(let touch of event.changedTouches){
-        start(touch)
+        start(touch,contexts[HAND_SYMBOL])
     }
 })
 
 element.addEventListener('touchmove',(event)=>{
     for(let touch of event.changedTouches){
-        move(touch)
+        move(touch,contexts[HAND_SYMBOL])
     }
 })
 
 element.addEventListener('touchend',(event)=>{
     for(let touch of event.changedTouches){
-        end(touch)
+        end(touch,contextxs[HAND_SYMBOL])
     }
 })
 
 element.addEventListener('touchcancel',(event)=>{
     for(let touch of event.changedTouches){
-        cancel(touch)
+        cancel(touch,contexts[HAND_SYMBOL])
     }
 })
 
 let start = (point,context)=>{
-    console.log(point.clientX,point.clientY)
+    context.startTime = Date.now();
+    context.startX = point.clientX;
+    context.startY = point.clientY;
+    context.isPan = false;
+    context.isTap = true;
+    context.isPress = false;
+    context.moves = [];
 }
 
 let move = (point,context)=>{
-    console.log(point.clientX,point.clientY)
+    context.moves.push({
+        x:point.clientX,
+        y:point.clientY,
+        t:Date.now()
+    });
+    if((point.clientX - context.startX) ** 2 + (point.clientY - context.startY) ** 2 > 100){
+        context.isTap = false;
+        context.isPress = false;
+        context.isPan = true;
+    }
 }
 
 let end = (point,context)=>{
-    console.log(point.clientX,point.clientY)
+    if(context.isPan){
+        context.moves = context.moves.filter(item => Date.now() - item.t < 300);
+        console.log(context.moves);
+        let event = new CustomEvent('pan',{
+            detail:{
+                x:context.startX,
+                y:context.startY,
+                t:Date.now()-context.startTime
+            }
+        });
+        console.log('panend');
+    }else if(Date.now() - context.startTime > 500){
+        context.isPan = false;
+        context.isTap = false;
+        context.isPress = true;
+    }
+    if(context.isPress){
+        console.log('pressend');
+    }
+    if(context.isTap){
+        console.log('tapend');
+    }
 }
 
 let cancel = (point,context)=>{
